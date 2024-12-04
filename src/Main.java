dd3a86662f6d8618a717fa3a162f58976194e14c
programmingparadox <theparadox31415@gmail.com>
2024-12-04 15:03:15 -0800

    basic mandelbrot rendering

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,21 @@ public class Main extends JPanel implements Runnable, MouseListener {
 
     Thread t;
 
-    public int frameWidth = 1000;
+    public int frameWidth = 800;
     public int frameHeight = 800;
 
-    public int[] fill = new int[]{0, 0, 0, 255};
-    public int[] stroke = new int[]{0, 0, 0, 255};
-    public int strokeWeight = 1;
+    // viewport max to hold whole fractal nicely
+    double X_SCALE_MIN = -2.25;
+    double X_SCALE_MAX = 0.75;
+
+    double Y_SCALE_MIN = -1.5;
+    double Y_SCALE_MAX = 1.5;
+
+    // viewport size init to whole fractal
+    double x_view_min = X_SCALE_MIN;
+    double y_view_min = Y_SCALE_MIN;
+    double x_view_max = X_SCALE_MAX;
+    double y_view_max = Y_SCALE_MAX;
 
     public String fontName = "Trebuchet MS";
     public int fontSize = 12;
@@ -26,6 +35,8 @@ public class Main extends JPanel implements Runnable, MouseListener {
 
     public Main(String name, int w, int h){
         frame = new JFrame(name);
+
+        img = new BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB);
 
         frameWidth = w;
         frameHeight = h;
@@ -41,6 +52,8 @@ public class Main extends JPanel implements Runnable, MouseListener {
     public Main(String name){
         frame = new JFrame(name);
 
+        img = new BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB);
+
         frame.add(this);
         frame.setSize(frameWidth, frameHeight);
         frame.setVisible(true);
@@ -51,6 +64,8 @@ public class Main extends JPanel implements Runnable, MouseListener {
     }
     public Main(){
         frame = new JFrame("GUI");
+
+        img = new BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB);
 
         frame.add(this);
         frame.setSize(frameWidth, frameHeight);
@@ -78,7 +93,7 @@ public class Main extends JPanel implements Runnable, MouseListener {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        Graphics2D g2d = (Graphics2D)g;
+        Graphics2D g2d = (Graphics2D) g;
 
         RenderingHints rh = new RenderingHints(
                 RenderingHints.KEY_TEXT_ANTIALIASING,
@@ -93,6 +108,46 @@ public class Main extends JPanel implements Runnable, MouseListener {
 
         WritableRaster data = img.getRaster();
 
+        int height = data.getHeight();
+        int width = data.getWidth();
+
+        double dh = 1.0 / height;
+        double dw = 1.0 / width;
+
+        double ww = (Math.abs(X_SCALE_MAX) + Math.abs(X_SCALE_MIN));
+        double hh = (Math.abs(Y_SCALE_MAX) + Math.abs(Y_SCALE_MIN));
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+
+                double scaledX = ((x * dw) * ww) + X_SCALE_MIN;
+                double scaledY = ((y * dh) * hh) + Y_SCALE_MIN;
+
+                //double scaledX = 0;
+                //double scaledY = 0;
+
+                double a = 0.0;
+                double b = 0.0;
+                int itr = 0;
+                while(
+                        (a * a) + (b * b) <= 4 &&
+                        itr < 1000 // (max itr)
+                ){
+                    double temp = (a * a) - (b * b) + scaledX;
+                    b = 2 * a * b + scaledY;
+                    a = temp;
+
+                    itr++;
+                }
+
+                itr = 1000 - itr;
+
+                int color = (int) (Math.log(itr) * Math.log(1000) * 255);
+
+                int[] fill = new int[]{color, color, color, 255};
+
+                data.setPixel(x, y, fill);
+            }
+        }
 
 
         // draw
